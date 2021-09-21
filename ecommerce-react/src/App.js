f46d261c959e68718a0bfc3e3ed3d636598c7d8a
bigote97 <72711721+bigote97@users.noreply.github.com>
2021-09-21 00:26:21 -0300

    Actualizacion de "Dashboard" a "Ecommerce""

---
+++ ecommerce-react/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <header>
+        <p className="text-verde-darseafoamk mx-6 my-3"> <span className="font-semibold text-xl tracking-tight"> GrEEnCommerce </span> es el primer vivero virtual creado por aficionados, con el fin de recaudar lo necesario para subsistir y fomentar el crecimiento de la cultura sustentable asi como el cuidado del medio ambiente cultivando plantas autoctonas y sistemas de biodiversidad que colaboren en la reincorporacion de los recursos al medio ambiente</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce-react/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      verde: {
+        spearmint: '#94C973',
+        darseafoamk: '#2F5233',
+        oscuro: '#1A4314',
+        kelly: '#B1D8B7',
+        lime: '#76B947',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
