cbb1ef460009af3cb4eb57177ca7b724852859e5
bigote97 <72711721+bigote97@users.noreply.github.com>
2021-09-30 16:38:51 -0300

    Promesas y efecto de carga

--- ecommerce-react/src/App.js
+++ ecommerce-react/src/App.js
@@ -5,150 +5,22 @@ import SubTitulos from "./components/SubTitulos";
 
 import ListadoProductos from "./components/products/ListadoProductos";
 
-const Herramientas = [
-	{
-		titulo: 'Rastrillo de alambre',
-		precio: 49.90,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS_Oj1daWnwIlFGHxLVx46VQz-OfMY7VauYAyPonbl7i0RK8o_CuhKQ3OrPiXIldqqTmI&usqp=CAU',
-		alt: 'Rastrillo de alambre',
-		id: "21",
-		stock: 10
-	},
-	{
-		titulo: 'Set pala y tridente',
-		precio: 35.00,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://2.bp.blogspot.com/-ym1bD77zh2k/WGPm5djH-MI/AAAAAAABRIo/_vi_1MOXuiURqGx4fNqWtkk7hRhWd8VewCEw/s1600/000%2BMis%2Bherramientas%2Bde%2Bjard%25C3%25ADn%2B%252827%2529%2B.jpg',
-		alt: 'Set pala y tridente',
-		id: "22",
-		stock: 20
-	},
-	{
-		titulo: 'Regadera de zinc',
-		precio: 140.50,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'http://catalogos.devenado.ar/contenidos/19620_1.jpg',
-		alt: 'Regadera de zinc',
-		id: "23",
-		stock: 33
-	},
-	{
-		titulo: 'Aspersor 360°',
-		precio: 40.50,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Q8dLrqwGd9285skWXLDD1mw6YFla2CZLTg&usqp=CAU',
-		alt: 'Aspersor 360°',
-		id: "24",
-		stock: 5
-	}
-]
+import productos from "./data/productos";
+import titulos from "./data/titulos";
 
-const Invernaderos = [
-	{
-		titulo: 'Invernadero 5x10 metros',
-		precio: 49.90,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9xXpl8bMcvlUvGzHYL1E1Qos9XpJ7ajUpWg&usqp=CAU',
-		alt: 'Invernadero 5x10 metros',
-		id: "31",
-		stock: 10
-	},
-	{
-		titulo: 'Invernadero 3x5 metros',
-		precio: 35.00,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://www.hideouthouse.com/wp-content/uploads/2015/05/solar-panel-greenhouse.jpg',
-		alt: 'Invernadero 3x5 metros',
-		id: "32",
-		stock: 10
-	},
-	{
-		titulo: 'Torres de cultivo',
-		precio: 40.50,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://pacificdomes.com/WP/wp-content/uploads/2021/02/interior-greenhouse-dome.jpg',
-		alt: 'Torres de cultivo',
-		id: "33",
-		stock: 10
-	},
-	{
-		titulo: 'Invernadero domo',
-		precio: 40.50,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtAU3XoKaTesL3M970RbUFaetZ4h-FEHgliw&usqp=CAU',
-		alt: 'Invernadero domo',
-		id: "34",
-		stock: 10
-	}
-]
 
-const Plantas = [
-	{
-		titulo: 'Oregano',
-		precio: 49.90,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIGAWIMOsb3HqwvtRjbT8kiyFE3LpE03rwEg&usqp=CAU',
-		alt: 'Planta de Oregano',
-		id: "11",
-		stock: 10
-	},
-	{
-		titulo: 'Menta',
-		precio: 35.00,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQwr5ay4-BSumj6brRZgGJlCGudhA_uuLbhw&usqp=CAU',
-		alt: 'planta de Menta',
-		id: "12",
-		stock: 10
-	},
-	{
-		titulo: 'Albahaca',
-		precio: 40.50,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzNkXf1uJjcS4pUPKZabI2QM1FL6DhHC_xUg&usqp=CAU',
-		alt: 'Planta de Albahaca',
-		id: "13",
-		stock: 10
-	},
-	{
-		titulo: 'Tomillo',
-		precio: 45.50,
-		url: 'https://tailwindui.com/components/ecommerce/components/product-lists',
-		imagen: 'https://www.radiopopularsanluis.com.ar/u/fotografias/m/2021/2/10/f800x450-99174_150620_156.jpg',
-		alt: 'Planta de Tomillo',
-		id: "14",
-		stock: 10
-	}
-]
-
-const subtituloPlantas = {
-	id:"ListadoPlantas",
-	subtitulo: "Plantas",
-	adicional: "Plantas es la seccion donde encontraras todas las especies con las que disponemos actualmente"
-};
-const subtitulosHerramientas = {
-	id:"ListadoHerramientas",
-	subtitulo: "Herramientas",
-	adicional: "Herramientas es la seccion donde encontraras todo lo necesario para trabajar tu huerta y tu jardin"
-};
-const subtitulosInvernaderos = {
-	id: "ListadoInvernaderos",
-	subtitulo: "Invernaderos",
-	adicional: "Contamos con varios modelos de invernaderos y elementos para su optima utilizacion"
-};
 function App() {
   return (
     <div>
       <NavBar />
-      <Cuerpo />
-	  <SubTitulos texto={subtituloPlantas}/>
-      	<ListadoProductos productos={Plantas} />
-	  <SubTitulos texto={subtitulosHerramientas}/>
-      	<ListadoProductos productos={Herramientas} />
-	  <SubTitulos texto={subtitulosInvernaderos}/>
-      	<ListadoProductos productos={Invernaderos} />
-      <Footer />
+		<Cuerpo/>
+		<SubTitulos texto={titulos.subtituloPlantas}/>
+			<ListadoProductos productos={productos.Plantas} />
+		<SubTitulos texto={titulos.subtitulosHerramientas}/>
+			<ListadoProductos productos={productos.Herramientas} />
+		<SubTitulos texto={titulos.subtitulosInvernaderos}/>
+			<ListadoProductos productos={productos.Invernaderos} />
+      	<Footer />
     </div>
   );
 }
---
+++ ecommerce-react/src/data/titulos.js
@@ -0,0 +1,17 @@
+export default {
+    "subtituloPlantas": {
+        "id":"ListadoPlantas",
+        "subtitulo": "Plantas",
+        "adicional": "Plantas es la seccion donde encontraras todas las especies con las que disponemos actualmente"
+    },
+    "subtitulosHerramientas": {
+        "id":"ListadoHerramientas",
+        "subtitulo": "Herramientas",
+        "adicional": "Herramientas es la seccion donde encontraras todo lo necesario para trabajar tu huerta y tu jardin"
+    },
+    "subtitulosInvernaderos": {
+        "id": "ListadoInvernaderos",
+        "subtitulo": "Invernaderos",
+        "adicional": "Contamos con varios modelos de invernaderos y elementos para su optima utilizacion"
+    }
+}
